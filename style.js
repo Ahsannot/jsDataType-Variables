7df5b8a40e0a99d77e1cc9531c24f022ae70da1d
Ahsannot <hafizgee07@gmail.com>
2024-01-23 23:23:53 +0500

    js Variables and Data Types

---
+++ style.js
@@ -0,0 +1,18 @@
+
+let fullName = "Ahsan Ali khan";
+let age = 32;
+let married = true;
+
+let family = {
+     fatherName : "Hafiz Muhammad Ahsan",
+     fatherAge : 32,
+     fatherCar : false,
+};
+
+
+console.log(fullName);
+console.log(age);
+console.log(married);
+console.log("Welcome To JavaScript Ahsan");
+
+console.log(family);
